27b104da913e0fc9fb52cb7e13eee58890d69c8b
Liquidmetal6 <leiterm@purdue.edu>
2014-02-22 02:09:19 -0500

    leaving ooff of the load file. don't know how to read into a linked list

--- PA2/sorting.c
+++ PA2/sorting.c
@@ -4,11 +4,12 @@
 #include<time.h>
 
 ///////////////////////////////
-Node* List_Create(long int value,struct node* next);
+Node* List_Create(long int value,struct node* next)
 {
   Node * ln = malloc(sizeof(Node));
   ln->value = value;
-  ln->next = NULL;
+  ln->next = next;
+  return ln;
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -17,14 +18,22 @@ Node* Load_File(char *Filename)
   //Open file
   FILE* fptr = fopen(Filename, "r");
 
-  //Validity Check
+  //Validity Check, return 0 if unsuccesful
   if(fptr ==NULL)
-    {printf("File didnt open!"); return NULL);
+    {printf("File didnt open!"); return 0;}
+    
+  //while not at the end of the file, scan into a function that returns a node
+  while(!feof(fptr))
+    {
+      int scanreturn = 0;
+      //Leaving off of here, how to fscanf into a linked list?  scanreturn = fscanf(fp,
+    }
 
 
-
- 
+  return(0);
 }
+
+
 
 
 
